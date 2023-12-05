70872e0bf8c4b89fd0d5b2cf92a57bedb31f7093
leehaeseong1219 <84298862+leehaeseong1219@users.noreply.github.com>
2023-12-05 17:30:56 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,3 +22,5 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+
+java -jar /home/ec2-user/jenkins/build/libs/demo-0.0.1-SNAPSHOT.jar
